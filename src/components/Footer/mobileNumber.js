a2cb95cc9a7d57ab9546df5b7a694dbf724c48f7
Ratan Sunder Parai <ratanparai@gmail.com>
2022-04-02 12:25:02 +0600

    phone number link

--- src/components/Footer/mobileNumber.js
+++ src/components/Footer/mobileNumber.js
@@ -3,7 +3,11 @@ import React from 'react'
 const MobileNumber = () => {
   return (
     <div className='font-courier-new'>
-      +88 01717 024050
+      <a
+        href='tel:+8801717024050'>
+
+        +88 01717 024050
+      </a>
     </div>
   )
 }
